e708e216f3ea26cad9b1ac8d8f284759442d2476
mariusgronli <mariusgronli@gmail.com>
2020-03-30 14:40:27 +0100

    Added permissons and token auth

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('api-viewset',views.HelloViewSet, base_name='api-viewset')
+router.register('profile-viewset',views.UserProfileViewSet)
 
 urlpatterns=[
     path('hello-view/', views.HelloApiView.as_view()),
